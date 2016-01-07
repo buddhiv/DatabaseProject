57e8b030a7f786738b8d56e09de60c84b52e88a8
SupunArunoda <supunarunoda@gmail.com>
2016-01-07 12:02:28 +0530

    Ministry Views are completed

---
+++ ministry_views/progress.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Supun
+ * Date: 1/7/2016
+ * Time: 11:57 AM
+ */
--- php/mysql_connector.php
+++ php/mysql_connector.php
@@ -1,23 +1,15 @@
 <?php
-
 function getConnection()
 {
     $db_host = "localhost";
     $db_username = "root";
     $db_password = "";
-
     $db_name = "grade_one";
 
-    $db_name = "database_project";
-
-
     $connection = mysqli_connect("$db_host", "$db_username", "$db_password", "$db_name");
-
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-
     return $connection;
 }
-
 ?>
