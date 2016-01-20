92948495f8ead63723c3ecb1adcbe77b6a165d2b
sugeesh <lpdneo@gmail.com>
2016-01-20 19:24:19 +0530

    Wrong parts corrected

--- school_views/get_teachers_from_school.php
+++ school_views/get_teachers_from_school.php
@@ -14,11 +14,11 @@ $teacherController = new TeacherController();
 
 if (isset($_GET['school_id'])) {
     if ($_GET['school_id'] != "") {
-        $teachers = $teacherController->getTeachersBySchool($_GET['school_id']);
+        $teachers = $teacherController->getLeavedTeachersBySchool($_GET['school_id']);
 
         echo '<option></option>';
         foreach ($teachers as $teacher) {
-            echo '<option value="' . $teacher['student_id'] . '">' . $teacher['number'] . " - " . $teacher['name_in_full'] . '</option>';
+            echo '<option value="' . $teacher['teacher_id'] . '">' .$teacher['name_in_full'] . '</option>';
         }
     }
 }
